5c78540ba2dc9dcd3a79a15840723d22a8babd35
AlexYu01 <tengalexyu@gmail.com>
2018-09-18 09:28:30 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,9 @@ int print_last_digit(int n)
 {
 	int last;
 
+	last = n % 10;
 	if (n < 0)
-	{
-		last = -n % 10;
-	}
-	else
-	{
-		last = n % 10;
-	}
+		last *= -1;
 	_putchar(last + '0');
 	return (last);
 }
