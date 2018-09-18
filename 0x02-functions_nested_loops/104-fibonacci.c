09c0450fba2d8d050a6fd807fda0839b4d7c693a
AlexYu01 <tengalexyu@gmail.com>
2018-09-18 18:28:04 +0000

    Working properly

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,23 +9,53 @@
 int main(void)
 {
 	int i;
+	
 	long double first;
 	long double second;
-	long double new;
+	long double old_carry;
+	long double new_carry;
+	long double cutoff;
+	long double sum;
+	long double temp;
 
+	cutoff = 1000000000000000000;
+	old_carry = new_carry = 0;
 	first = 1;
 	second = 2;
-//	printf("%.0f, %.0f, ", first, second);
-	puts(first);
-	puts(", ");
-	puts(second);
-	put(", ");
+	printf("%.0Lf, %.0Lf, ", first, second);
 	for (i = 0; i < 98; i++)
 	{
-		new = second;
-		second += first;
-		first = new;
-		puts(second);
+		sum = second + first;
+		if (sum > cutoff || new_carry > 0)
+		{
+			if (old_carry == 0 && new_carry == 0)
+			{
+				first = second;
+			}
+			else
+			{
+				first = second;
+				temp = old_carry;
+				old_carry = new_carry;
+			}
+			if (sum > cutoff)
+			{
+				second = sum - cutoff;
+				new_carry = new_carry + 1;
+			}
+			else
+			{
+				second = sum;
+			}
+			new_carry = temp + new_carry;
+			printf("%.0Lf%.0Lf", new_carry, second);
+		}
+		else
+		{
+			first = second;
+			second = sum;
+			printf("%.0Lf", second);
+		}
 		if (i < 97)
 		{
 			printf(", ");
