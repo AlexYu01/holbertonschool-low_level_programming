c417fa7651c1b5786f64ceb4fdb9d66d4cffe6c6
AlexYu01 <tengalexyu@gmail.com>
2018-09-18 01:59:50 +0000

    Split function due to exceeding 40 lines per function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,10 @@
 #include "holberton.h"
 
+void times_table_helper(int digit1, int digit2);
+
 /**
- * times_table - Prints multiplication table starting from 0 to 9
+ * print_times_table - Prints multiplication table starting from 0 to 9
+ * @n: later
  *
  * Return: void
  */
@@ -25,32 +28,47 @@ void print_times_table(int n)
 				digit1 = prod / 100;
 				digit2 = (prod % 100) / 10;
 				digit3 = (prod % 100) % 10;
-
-				if ((digit1 != 0) && (horz != 0))
+				if (horz != 0)
 				{
-					_putchar(' ');
-					_putchar(digit1 + '0');
-					_putchar(digit2 + '0');
-				}
-				else if ((digit2 != 0) && (horz != 0))
-				{ 
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(digit2 + '0');
-				}
-				else if (horz != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					times_table_helper(digit1, digit2);
 				}
 				_putchar(digit3 + '0');
-				if ( horz != n)
+				if (horz != n)
 				{
 					_putchar(',');
 				}
 			}
 			_putchar('\n');
 		}
+	}
+}
+/**
+ * times_table_helper - Helper functions for print_times_table function.
+ * Prints the needed number of spaces depending on the value of digit1
+ * and digit2.
+ * @digit1: First digit of the product in a number represented in three
+ * digits.
+ * @digit2: Second digit of the product in a number represented in three
+ * digits.
+ *
+ * Return: void
+ */
+void times_table_helper(int digit1, int digit2)
+{
+	_putchar(' ');
+	if (digit1 != 0)
+	{
+		_putchar(digit1 + '0');
+		_putchar(digit2 + '0');
+	}
+	else if (digit2 != 0)
+	{
+		_putchar(' ');
+		_putchar(digit2 + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(' ');
 	}
 }
