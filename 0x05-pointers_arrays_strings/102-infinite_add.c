8302796373c9ed13b9b553d62c7206aeb7f22376
AlexYu01 <tengalexyu@gmail.com>
2018-09-28 01:05:22 +0000

    Adding task 12

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 void rev_string(char *s);
 
 /**
---
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * print_buffer - Prints a buffer.
+ * @b: Pointer to a string.
+ * @size: Size of the buffer.
+ *
+ * Return: void.
+ */
+
+void print_buffer(char *b, int size)
+{
+	int firstc = 0;
+	int charc = 0;
+	int i, k, l;
+
+	if (size == 0)
+		printf("\n");
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (k = 0; k < 5; k++)
+		{
+			for (l = 0; l < 2; l++)
+			{
+				if (firstc < size)
+					printf("%02x", b[firstc++]);
+				else
+					printf("  ");
+			}
+			printf(" ");
+		}
+		for (; charc < i + 10; charc++)
+		{
+			if (b[charc] > 31)
+				printf("%c", b[charc]);
+			else if (charc < size)
+				printf(".");
+		}
+		printf("\n");
+	}
+}
