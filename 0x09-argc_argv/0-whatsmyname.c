b0c475727afa0e44a7b6c3a0188c82815b47a4ee
AlexYu01 <tengalexyu@gmail.com>
2018-10-04 19:06:35 +0000

    Commit task 0: print name of program

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+  * main - Prints the name of the program.
+  * @argc: The number of arguments.
+  * @argv: An array of pointers to strings containing the arguments.
+  *
+  * Return: 0 upon success.
+  */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	
+	printf("%s\n", *argv);
+
+	return (0);
+}
