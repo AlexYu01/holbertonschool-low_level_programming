13267eab3e34750a689d16e34dfda6e0c913992b
AlexYu01 <tengalexyu@gmail.com>
2018-10-09 19:35:10 +0000

    Commit task 3: Create 2D array of integers using malloc

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+/**
+  * alloc_grid - Returns a pointer to a 2 dimensional array of integers.
+  * @width: Width of the array.
+  * @height: Height of the array.
+  *
+  * Return: A pointer to a 2 dimensional array.
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i;
+	int j;
+
+	if (height < 0 || width < 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(*ptr) * height);
+
+	if (ptr)
+	{
+		for (i = 0; i < height; i++)
+			ptr[i] = malloc(sizeof(**ptr) * width);
+		if (*ptr)
+		{
+			for (i = 0; i < height; i++)
+			{
+				for (j = 0; j < width; j++)
+					ptr[i][j] = 0;
+			}
+		}
+	}
+
+	return (ptr);
+}
