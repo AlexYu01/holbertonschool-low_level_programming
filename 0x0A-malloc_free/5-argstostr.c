cb9f447d73b2205086fa183225b2a6ec5a3e776c
AlexYu01 <tengalexyu@gmail.com>
2018-10-09 19:47:55 +0000

    Commit task 5: Concatenates all arguments to a program

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+
+unsigned int _strlen(char *s);
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to the characters string.
+ *
+ * Return: The length of the character string.
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int length;
+
+	length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
+/**
+  * argstostr - Concatenates all the arguments of the program.
+  * @ac: The number of arguments.
+  * @av: Pointer to a pointer of strings containing the arguments.
+  *
+  * Return: Pointer to the concatenated argument string.
+  */
+
+char *argstostr(int ac, char **av)
+{
+	unsigned int len;
+	int i;
+	int j;
+	int index;
+	char *ptr;
+
+	len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		len += _strlen(av[i]);
+
+	ptr = malloc(sizeof(**av) * (len + 1));
+
+	if (ptr)
+	{
+		for (i = 0; i < ac; i++)
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				ptr[index++] = av[i][j];
+				j++;
+			}
+			if (!(av[i][j]))
+				ptr[index++] = '\n';
+		}
+
+		ptr[index] = '\0';
+	}
+
+	return (ptr);
+}
