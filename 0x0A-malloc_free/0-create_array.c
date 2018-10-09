3159fcd4f392e487efb766864e6e91bec9d46f17
AlexYu01 <tengalexyu@gmail.com>
2018-10-09 19:21:58 +0000

    Commit task 0: Create and fill array with char

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+  * create_array - Creates an array of chars, and initialized it with a
+  * specific char.
+  * @size: Unsigned int that represents the size of the array.
+  * @c: The character to fill the array with.
+  *
+  * Return: A char pointer to the array.
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc(sizeof(size) * size);
+
+	if (ptr)
+	{
+		for (i = 0; i < size; i++)
+			ptr[i] = c;
+	}
+
+	return (ptr);
+}
