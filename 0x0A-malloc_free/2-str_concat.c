11a3fa5615c0872e7fca291613b5d1344289bf94
AlexYu01 <tengalexyu@gmail.com>
2018-10-09 19:33:41 +0000

    Set input strings to empty if they are null

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -37,6 +37,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int j;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	ptr = malloc(sizeof(*s1) * (len1 + len2 + 1));
