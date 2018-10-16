81f310a01cfa4a3172c6f1da16618c9d99665bed
AlexYu01 <tengalexyu@gmail.com>
2018-10-16 01:32:10 +0000

    Commit task 5: Free up memory used by struct dog

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * free_dog - Frees up heap memory consumed by the variable d points to.
+  * @d: Pointer to a structure that uses heap memory.
+  *
+  * Return: void.
+  */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+	}
+}
