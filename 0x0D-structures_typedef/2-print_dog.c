c3aff5bc620127f3762ec6069bb6131fa60254c3
AlexYu01 <tengalexyu@gmail.com>
2018-10-16 01:11:56 +0000

    Commit task 2: Prints a struct dog

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * print_dog - Prints the information store in the structure, dog.
+  * @d: Pointer an instance of structure dog.
+  *
+  * Return: void.
+  */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: ");
+		if (d->name)
+			printf("%s\n", d->name);
+		else
+			printf("(nil)\n");
+
+		printf("Age: ");
+		if (d->age)
+			printf("%f\n", d->age);
+		else
+			printf("(nil)\n");
+		printf("Ownder: ");
+		if (d->owner)
+			printf("%s\n", d->owner);
+		else
+			printf("(nil)\n");
+	}
+}
