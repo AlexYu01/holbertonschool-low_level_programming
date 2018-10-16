964d08c2fe1d7f4aef0c1ff2ee57190f64cca4c0
AlexYu01 <tengalexyu@gmail.com>
2018-10-16 01:50:22 +0000

    Deep copy of name and owner

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,44 @@
 #include "dog.h"
 
 /**
+ * _strlen- Returns the length of a string.
+ * @s: A pointer to the character string.
+ *
+ * Return: The length of the character string.
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int length;
+
+	length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
+
+/**
+ * _strcpy - Copies the string pointed to by src, including the terminating
+ * null byte to the buffer pointed by des.
+ * @dest: Pointer to the destination of copied string.
+ * @src: Pointer to the src of the source string.
+ *
+ * Return: void.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
+/**
   * new_dog - Creates a new dog.
   * @name: Name of the new dog.
   * @age: Age of the new dog.
@@ -12,15 +50,28 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *dog;
+	dog_t *dog;
 
 	dog = malloc(sizeof(*dog));
 	if (!dog)
 		return (NULL);
 
-	dog->name = name;
+	dog->name = malloc(sizeof(*name) * (_strlen(name) + 1));
+	if (!(dog->name))
+	{
+		free(dog);
+		return (NULL);
+	}
+	
 	dog->age = age;
-	dog->owner = owner;
+	
+	dog->owner = malloc(sizeof(*owner) * (_strlen(owner) + 1));
+	if (!(dog->owner))
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 
 	return (dog);
 }
