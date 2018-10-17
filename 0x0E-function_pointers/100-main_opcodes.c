4c7bea136c7145530569e81742a34e0827547480
AlexYu01 <tengalexyu@gmail.com>
2018-10-17 22:10:52 +0000

    Commit task 4: Print opcodes of the main function

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Print the opcodes of its own main function.
+  * @argc: The number of arguments passed to the function.
+  * @argv: Pointer to pointers containing the arguments.
+  *
+  * Return: 0 upon successful run.
+  */
+
+int main(int argc, char **argv)
+{
+	int num_bytes;
+	int hex;
+	int i;
+	int (*fp)(int, char **);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	fp = main;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		hex = *(char *)(fp + i);
+		if (hex > 0xffffff00)
+			hex -= 0xffffff00;
+		printf("%02x", hex);
+		if (i < num_bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
+
