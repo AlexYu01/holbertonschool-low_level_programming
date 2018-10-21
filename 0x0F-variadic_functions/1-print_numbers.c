061dff5ad0dbc72da1a76e7ba26b46718c9dc5f0
AlexYu01 <tengalexyu@gmail.com>
2018-10-21 04:46:26 +0000

    Print new line if n is 0

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -16,22 +16,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 
 	va_start(nums, n);
 
-	if (separator)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n - 1; i++)
-			printf("%d%s", va_arg(nums, int), separator);
-		printf("%d\n", va_arg(nums, int));
+
+		printf("%d", va_arg(nums, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-			printf("%d", va_arg(nums, int));
-		printf("\n");
-	}
+	printf("\n");
 
 	va_end(nums);
 }
