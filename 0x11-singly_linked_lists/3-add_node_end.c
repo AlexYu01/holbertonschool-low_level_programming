7a1114308a4a711e31b82075bee4aa3574872ae4
AlexYu01 <tengalexyu@gmail.com>
2018-10-25 19:09:45 +0000

    Protect strdup from NULL string

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -37,6 +37,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *ptr;
 
+	if (!str)
+		return (NULL);
+
 	ptr = *head;
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
