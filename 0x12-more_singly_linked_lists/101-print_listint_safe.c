a9a54c3971d9ec005c2218484c50b9ac582a77fe
AlexYu01 <tengalexyu@gmail.com>
2018-10-29 21:43:38 +0000

    Commit task 12: Safe version of printing a linked list

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - Prints a lintint_t linked list.
+ * @head: const listint_t pointer to the beginning of the list.
+ *
+ * Return: the size of the list.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int count;
+	const listint_t *ptr;
+
+	count = 0;
+	ptr = head;
+	if (!head)
+		exit (98); /* not sure to exit 98 here. */
+	while (ptr)
+	{
+		printf("[%p] %d\n", (void *) ptr, ptr->n);
+		count++;
+		if (ptr->next && ((void *) ptr->next < (void *) ptr))
+			ptr = ptr->next;
+		else
+			break;
+	}
+	if (ptr->next)
+	{
+		ptr = ptr->next;
+		printf("-> [%p] %d\n", (void *) ptr, ptr->n);
+	}
+	return (count);
+}
