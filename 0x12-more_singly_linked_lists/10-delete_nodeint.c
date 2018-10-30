959da31bd590778febe72b53c751a274c68b5c20
AlexYu01 <tengalexyu@gmail.com>
2018-10-30 16:27:33 +0000

    Return -1 instead of NULL

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int current_ind;
 
 	if (!head)
-		return (NULL);
+		return (-1);
 	current = *head;
 	current_ind = 0;
 
