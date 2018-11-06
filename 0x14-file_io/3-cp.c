9d2464b3195f7a8ac052b2b29415130bcc1a8670
AlexYu01 <tengalexyu@gmail.com>
2018-11-06 20:02:58 +0000

    Completed task 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 /**
  * main - Copies the content of a file to another file.
@@ -13,20 +14,44 @@
 
 int main(int argc, char **argv)
 {
-	ssize_t file_to;
-	ssize_t file from;
+	int file_to;
+	int file_from;
+	ssize_t b_read;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		close(file_from); /* may need to check close */
+		exit(99);
+	}
 
-	file_to = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	/*
-	   check file_to, if fail exit with 99 and print Error: Can't write to NAME_OF_THE_FILE
+	do {
+		b_read = read(file_from, buffer, 1024);
+		write(file_to, buffer, b_read);
+	} while (b_read >= 1024);
 
-
-
-	   check for close, if fail exit with code 100 and print Error: Can't close fd FD_VALUE
-	   */
+	if (close(file_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
