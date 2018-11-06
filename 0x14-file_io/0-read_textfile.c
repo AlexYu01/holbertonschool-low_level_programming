e5e2b5dcde930ce5e622a97579d7da852d82c9f7
AlexYu01 <tengalexyu@gmail.com>
2018-11-06 20:44:57 +0000

    Allocate exactly the number of bytes based on letters

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (file == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 	{
 		close(file);
@@ -41,7 +41,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-	buffer[letters] = '\0';
 
 	b_write = write(1, buffer, b_read);
 	free(buffer);
