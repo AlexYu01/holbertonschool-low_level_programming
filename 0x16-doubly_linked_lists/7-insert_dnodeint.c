7c68d4cf0990384b64edd6f42bd08c07dbb850f4
AlexYu01 <tengalexyu@gmail.com>
2018-12-10 00:32:55 +0000

    Overhaul the insertion process

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,4 @@
 #include "lists.h"
-/**
- * insert_node - Inserts the new node correctly into the list.
- *
- * @h: Double pointer to the beginning of the list.
- * @current: Pointer to the current node.
- * @new: Pointer to the new node.
- * @count: Current index of the node current.
- * @idx: Desired position of the new node.
- *
- * Return: void.
- */
-void insert_node(dlistint_t **h, dlistint_t *current, dlistint_t *new,
-		unsigned int count, unsigned int idx)
-{
-	if (current != NULL)
-	{
-		if (count != idx - 1)
-		{
-			if (current->prev != NULL)
-				(current->prev)->next = new;
-			new->prev = current->prev;
-			current->prev = new;
-		}
-		else
-		{
-			current->next = new;
-			new->next = NULL;
-			new->prev = current;
-		}
-	}
-	else
-	{
-		new->prev = NULL;
-		*h = new;
-	}
-}
 
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position of a
@@ -49,33 +13,41 @@ void insert_node(dlistint_t **h, dlistint_t *current, dlistint_t *new,
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
-	dlistint_t *current;
+	dlistint_t *before;
 	unsigned int count;
 
 	if (h == NULL)
 		return (NULL);
 
-	current = *h;
-	count = 0;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	while (current != NULL)
-	{
-		if (count == idx)
-			break;
-		current = current->next;
-		count++;
-	}
-
-	if (count != idx && count != idx - 1)
-		return (NULL);
-
+	before = *h;
+	count = 1;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
-	new->next = current;
-	insert_node(h, current, new, count, idx);
+
+	while (before != NULL)
+	{
+		if (count == idx)
+			break;
+		before = before->next;
+		count++;
+	}
+	if (count != idx)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = before->next;
+	new->prev = before;
+
+	if (before->next != NULL)
+		(before->next)->prev = new;
+	before->next = new;
 
 	return (new);
 }
