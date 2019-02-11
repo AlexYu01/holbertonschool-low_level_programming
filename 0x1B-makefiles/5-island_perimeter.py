15e1f7e82f4ce1b00e8f399a16079f4ee4d85962
AlexYu01 <tengalexyu@gmail.com>
2019-02-11 22:02:51 +0000

    Commit task 5: Calculate the perimeter of an island within a matrix

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    Module containing the `island_perimiter` function.
+"""
+
+
+def island_perimeter(grid):
+    """
+        Calculate the perimeter of the island in `grid`.
+
+        Args:
+            grid (:obj:`list` of :obj:`list` of int): A matrix list of lists
+                containing 0's and 1's to denote water and land.
+
+        Returns:
+            The perimeter of an island.
+    """
+    if not isinstance(grid, list):
+        return 0
+    r = len(grid)
+    if r <= 2:
+        return 0
+    if not isinstance(grid[0], list):
+        return 0
+    c = len(grid[0])
+    if c <= 2:
+        return 0
+
+    p = 0
+    for i in range(1, r - 1):
+        for j in range(1, c - 1):
+            if grid[i][j] is 1:
+                if grid[i][j - 1] is 0:
+                    p += 1
+                if grid[i][j + 1] is 0:
+                    p += 1
+                if grid[i - 1][j] is 0:
+                    p += 1
+                if grid[i + 1][j] is 0:
+                    p += 1
+    return p
