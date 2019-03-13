4bc383a2d28e66300460a7d2918a8a4f5a97c8ec
AlexYu01 <tengalexyu@gmail.com>
2019-03-13 23:56:20 +0000

    Revert

--- 0x1C-binary_trees/110-binary_tree_is_bst.c
+++ 0x1C-binary_trees/110-binary_tree_is_bst.c
@@ -33,11 +33,9 @@ char traverse(const binary_tree_t *node, int min, int max)
 	if (node == NULL)
 		return (1);
 
-	if (min == max)
+	if (node->n < min || node->n > max)
 		return (0);
 
-	if (node->n >= min && node->n <= max)
-		return (traverse(node->left, min, node->n) &&
-				traverse(node->right, node->n, max));
-	return (0);
+	return (traverse(node->left, min, node->n - 1) &&
+			traverse(node->right, node->n + 1, max));
 }
