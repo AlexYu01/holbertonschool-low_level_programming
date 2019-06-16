9f92f9c576d96145acf97fae6bd50e1770261659
AlexYu01 <tengalexyu@gmail.com>
2019-06-16 12:17:42 -0700

    Swap camera rotation and lateral movement functions
    - Swap camera rotation and lateral movmeent functios in files

--- maze/src/update.c
+++ maze/src/update.c
@@ -86,51 +86,41 @@ void move_backwd(map maze, player *status)
 }
 
 /**
- * rotate_right - Rotate the camera right.
+ * move_left - Move the player laterally left if allowed (no wall). Takes
+ * the perpendicular of dir_x and dir_y to acquire lateral distance.
  *
+ * @maze: Struct containing a pointer to the layout of the maze.
  * @status: Pointer to the struct containing variables on camera position.
  */
-void rotate_right(player *status)
+void move_left(map maze, player *status)
 {
-	double old_dir_x;
-	double old_plane_x;
-	double sine_val;
-	double cosine_val;
+	double dist_x;
+	double dist_y;
 
-	sine_val = sin(-status->rot_speed);
-	cosine_val = cos(-status->rot_speed);
-	old_dir_x = status->dir_x;
-
-	status->dir_x = status->dir_x * cosine_val - status->dir_y * sine_val;
-	status->dir_y = old_dir_x * sine_val + status->dir_y * cosine_val;
-	old_plane_x = status->camera_plane_x;
-	status->camera_plane_x = status->camera_plane_x * cosine_val -
-			status->camera_plane_y * sine_val;
-	status->camera_plane_y = old_plane_x * sine_val + status->camera_plane_y *
-			cosine_val;
+	dist_x = status->dir_y * status->move_speed;
+	dist_y = status->dir_x * status->move_speed;
+	if (maze.layout[(int)(status->pos_x - dist_x)][(int)status->pos_y] == '0')
+		status->pos_x -= dist_x;
+	if (maze.layout[(int)status->pos_x][(int)(status->pos_x + dist_y)] == '0')
+		status->pos_y += dist_y;
 }
 
 /**
- * rotate_left - Rotate the camera left.
+ * move_right - Move the player laterally right if allowed (no wall). Takes
+ * the perpendicular of dir_x and dir_y to acquire lateral distance.
  *
+ * @maze: Struct containing a pointer to the layout of the maze.
  * @status: Pointer to the struct containing variables on camera position.
  */
-void rotate_left(player *status)
+void move_right(map maze, player *status)
 {
-	double old_dir_x;
-	double old_plane_x;
-	double sine_val;
-	double cosine_val;
+	double dist_x;
+	double dist_y;
 
-	sine_val = sin(status->rot_speed);
-	cosine_val = cos(status->rot_speed);
-	old_dir_x = status->dir_x;
-
-	status->dir_x = status->dir_x * cosine_val - status->dir_y * sine_val;
-	status->dir_y = old_dir_x * sine_val + status->dir_y * cosine_val;
-	old_plane_x = status->camera_plane_x;
-	status->camera_plane_x = status->camera_plane_x * cosine_val -
-			status->camera_plane_y * sine_val;
-	status->camera_plane_y = old_plane_x * sine_val + status->camera_plane_y *
-			cosine_val;
+	dist_x = status->dir_y * status->move_speed;
+	dist_y = status->dir_x * status->move_speed;
+	if (maze.layout[(int)(status->pos_x + dist_x)][(int)status->pos_y] == '0')
+		status->pos_x += dist_x;
+	if (maze.layout[(int)status->pos_x][(int)(status->pos_y - dist_y)] == '0')
+		status->pos_y -= dist_y;
 }
